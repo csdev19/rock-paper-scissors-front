0d95feb8194fbf5dee2e051f181e48c7dbdbdbf1
cristiandev <cristiansotomayor.dev@gmail.com>
2021-11-14 22:46:11 -0500

    update layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import HelloWorld from '../components/HelloWorld';
-import HelloWorldGreet from '../modules/greet/components/HelloWorldGreet';
+import GameLayout from '../modules/game/layout';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <HelloWorld />
-        <HelloWorldGreet />
+        <GameLayout />
       </main>
 
       <footer className={styles.footer}>
